957f5de681b01d2b99a4c13c3b8c1c9cbe6514ee
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2021-08-19 12:26:00 +0200

    Add Header

---
+++ src/components/layout/Header/Header.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import clsx from 'clsx';
+import styles from './Header.module.scss';
+// import { connect } from 'react-redux';
+// import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+
+
+const Component = ({className, children}) => {
+  const classes = useStyles();
+  return (
+    <div className={clsx(className, styles.root)}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+Component.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+};
+
+// const mapStateToProps = state => ({
+//   someProp: reduxSelector(state),
+// });
+
+// const mapDispatchToProps = dispatch => ({
+//   someAction: arg => dispatch(reduxActionCreator(arg)),
+// });
+
+// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+
+export {
+  Component as Header,
+  // Container as Header,
+  Component as HeaderComponent,
+};
